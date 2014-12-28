a8a7ed6cf6448a111aa9d11cedf17f59a62a7dcc
jiangtao <jiangtao@tao-studio.net>
2014-12-28 19:29:09 +0800

    add t1

---
+++ tests/time/t1/t1.cpp
@@ -0,0 +1,21 @@
+// -*- C++ -*-
+//===========================================================================
+/* Time-stamp: <2014-12-28 19:27:11 by jiangtao> */
+
+/* @file t1.cpp
+ * $Id: t1.cpp 2014-12-28 11:26:34 jiangtao Exp $
+ *
+ * @author:Stone Jiang<jiangtao@tao-studio.net>
+ */
+//===========================================================================
+
+
+#include <iostream>
+
+int main ()
+{
+	std::cout << "hello, world.\n";
+	return 0;
+}
+
+
